c46e30b4d9d02ebb4c89bc13d9289d49f02d372d
Delsa Marasha <deemarasha77@gmail.com>
2023-02-26 02:42:59 -0800

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
 * main - Entry point
-* A C program that prints with put function
-*
 * Return: Always 0 (Success)
 */
 
 int main(void)
-{
+{	
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
