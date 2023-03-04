3c0605771efd6ba36e5f15d3e342a815269eee75
Delsa Marasha <deemarasha77@gmail.com>
2023-03-04 04:53:02 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *     Prints "Hello, world!" to the standard output
+ *
+ * Return:
+ *     Always 0 (Success)
+ */
+int main(void)
+{
+printf("Hello, world!\n");
+return (0);
+}
