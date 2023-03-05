d43f83da41fe86f9f0fb9772884e592ba8a1b0b0
Delsa Marasha <deemarasha77@gmail.com>
2023-03-05 12:32:29 -0800

    7. Intel

--- 0x00-hello_world/generate_asm.sh
+++ 0x00-hello_world/generate_asm.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Set the C file name
-CFILE="100-intel"
+export CFILE=main.c
 
 # Generate the assembly code using GCC
-gcc -S -masm=intel "$CFILE.c" -o "$CFILE.s"
+gcc -S -masm=intel "$CFILE" -o "${CFILE%.*}.s"
 
 # Check if the assembly code was generated successfully
 if [ $? -eq 0 ]; then
 echo "Assembly code generated successfully!"
 else
 echo "Error generating assembly code!"
+exit 1
 fi
+
+# Assemble and link the assembly code and execute the resulting binary
+gcc "${CFILE%.*}.s" -o "${CFILE%.*}" && "./${CFILE%.*}"
