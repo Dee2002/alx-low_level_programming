efee7e7b5955a10f7ac6f0885e27ef9517e33d41
Delsa Marasha <deemarasha77@gmail.com>
2023-03-06 16:45:06 -0800

    2. strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void _puts(char *str);
 void print_rev(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *my_strchr(char *s, char c);
 
 #endif /* MAIN_H */
