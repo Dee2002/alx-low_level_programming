3bb774658e79d8b6db9e6ef2d13d6f20c7e19e35
Delsa Marasha <deemarasha77@gmail.com>
2023-03-06 16:11:56 -0800

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
