c99df812adb4cdbc1960150ce78307f03820cd86
Delsa Marasha <deemarasha77@gmail.com>
2023-03-08 22:19:12 -0800

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,6 +9,8 @@
 */
 int is_prime_number(int n)
 {
+int i;
+
 if (n <= 1)
 return (0);
 
@@ -18,9 +20,9 @@ return (1);
 if (n % 2 == 0 || n % 3 == 0)
 return (0);
 
-int i = 5;
+i = 5;
 
-while (i * i <= n)
+while (i *i <= n)
 {
 if (n % i == 0 || n % (i + 2) == 0)
 return (0);
