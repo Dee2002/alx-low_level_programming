b09b5817f718c163e29372447e56f03d131cbc26
Dee2002 <deemarasha77@gmail.com>
2023-05-26 23:50:28 -0700

    13. Free (safe version) 0.2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,66 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-* free_listint_safe - Frees a listint_t linked list.
-* @h: Double pointer to the head of the list.
+* free_listint_safe - Frees a listint_t list safely.
+* @h: A pointer to the head of the list.
 *
 * Return: The size of the list that was freed.
 */
 size_t free_listint_safe(listint_t **h)
 {
+listint_t *slow, *fast, *temp;
 size_t count = 0;
-listint_t *current;
-listint_t *next;
 
 if (h == NULL || *h == NULL)
 return (0);
-current = *h;
-while (current != NULL)
+
+slow = *h;
+fast = (*h)->next;
+while (fast && fast->next && fast != slow)
 {
-next = current->next;
-free(current);
+temp = slow;
+slow = slow->next;
+fast = fast->next->next;
 count++;
-
-if (next >= current)
-break;
-
-current = next;
+if (slow == fast)
+{
+free_listint(*h);
+*h = NULL;
+return (count);
+}
 }
 
+if (slow == fast)
+{
+slow = slow->next;
+count++;
+while (slow != fast)
+{
+temp = slow;
+slow = slow->next;
+count++;
+}
+temp->next = NULL;
+}
+
+free_listint(*h);
 *h = NULL;
 
 return (count);
+}
+
+/**
+* free_listint - Helper function to free a listint_t list.
+* @head: A pointer to the head of the list.
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+while (head)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
 }
