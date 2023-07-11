b02c3bf7eb8f88ed6aace87a00da7052797963ed
Dee2002 <deemarasha77@gmail.com>
2023-07-10 17:06:11 -0700

    file change

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,30 +30,23 @@ char buffer[BUFFER_SIZE];
 
 if (argc != 3)
 error_exit("Usage: cp file_from file_to", 97);
-
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 error_exit("Error: Can't read from file", 98);
-
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 if (file_to == -1)
 error_exit("Error: Can't write to file", 99);
-
 while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(file_to, buffer, bytes_read);
 if (bytes_written != bytes_read || bytes_written == -1)
 error_exit("Error: Can't write to file", 99);
 }
-
 if (bytes_read == -1)
 error_exit("Error: Can't read from file", 98);
-
 if (close(file_from) == -1)
 error_exit("Error: Can't close fd", 100);
-
 if (close(file_to) == -1)
 error_exit("Error: Can't close fd", 100);
-
 return (0);
 }
